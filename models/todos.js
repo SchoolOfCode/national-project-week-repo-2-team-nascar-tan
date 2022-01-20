ccb57d4d263edcf9452315c18daf7d1e993d91a5
callumhemming <callum.m.hemming@outlook.com>
2022-01-20 23:28:09 +0000

    Fixed issues

--- models/todos.js
+++ models/todos.js
@@ -15,16 +15,16 @@ export async function getAllToDos(){
 }
 
 export async function getToDoListByUserIDandDate(userId, date){
-    return await query({text:"SELECT * FROM todos WHERE id=$1 AND date=$2 ORDER BY positionToDo", values:[userId, date]})
+    return await query({text:"SELECT * FROM todos WHERE id=$1 AND date=$2 ORDER BY position", values:[userId, date]})
 
 }
 
 
 
 
-export async function addNewToDoByIdAndDate(body){
+export async function addNewToDoByIdAndDate(body, id, date){
 
-    return await query({text:"INSERT INTO todos (userid, date, ToDoItem, checked, positionToDo) VALUES ($1, $2, $3, $4)", values:[]})
+    return await query({text:"INSERT INTO todos (id, date, item, checked, position) VALUES ($1, $2, $3, $4, $5)", values:[id, date, body.item, body.checked, body.position]})
 
 }
 
@@ -36,11 +36,12 @@ export async function updateToDoByIdAndDateAndPosition(id, date, position, body)
 
         switch(body.column){
             case "TODOITEM":
-            return await query({text:"UPDATE todos SET ToDoItem=$1 WHERE id=$1 AND date=$2 AND position=$3", values:[body.text,id,date, position ]})
+            return await query({text:"UPDATE todos SET item=$1 WHERE id=$1 AND date=$2 AND position=$3", values:[body.text,id,date, position ]})
             case "POSITION":
-            return await query({text:"UPDATE todos SET positionToDo=$1 WHERE id=$1 AND date=$2 AND position=$3", values:[body.text,id,date, position ]})
+            return await query({text:"UPDATE todos SET position=$1 WHERE id=$2 AND date=$3 AND position=$4", values:[body.text,id,date, position ]})
             case "CHECKED":
-            return await query({text:"UPDATE todos SET checked=$1 WHERE id=$1 AND date=$2 AND position=$3", values:[body.text,id,date, position ]})
+                // console.log(body.text)
+            return await query({text:"UPDATE todos SET checked=$1 WHERE id=$2 AND date=$3 AND position=$4", values:[body.text,id,date, position ]})
 
             default:
             return "Invalid column type in patch request, did you mean TITLE or BODY?"
@@ -50,6 +51,6 @@ export async function updateToDoByIdAndDateAndPosition(id, date, position, body)
 }
 
 export async function deleteToDoByIdAndDateAndPosition(id, date,position){
-    const response = await query({text:"DELETE FROM todos WHERE id=$1 AND date=$2 AND positionToDo=$3", values:[id, date,position]})
+    const response = await query({text:"DELETE FROM todos WHERE id=$1 AND date=$2 AND position=$3", values:[id, date,position]})
     return response
 }
